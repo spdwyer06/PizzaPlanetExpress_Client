24a711fb446fdad6f7e455966afb58e59c760742
Sean Dwyer <spdwyer06@gmail.com>
2021-01-26 01:21:26 -0500

    Had to reset AddToOrder and MenuItem, something went wrong during updates

--- src/Components/MenuItem/AddToOrder.tsx
+++ src/Components/MenuItem/AddToOrder.tsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input, Modal, ModalBody, ModalHeader, ModalFooter, Container, Row, Col } from "reactstrap";
 
-import API_URL from '../../env';
-
 
 
 type Props = {
-    token: string,
-    itemId: number,
-    orderId: number,
     addToOrderOn: boolean,
     toggleAddToOrder: () => void,
     updateSpecialInstructions: (instructions: string) => void,
     updateQuantity: (quantity: number) => void,
-    addItemToOrder: (e: React.FormEvent) => void
+    addItemToOrder: () => void
 };
 
 export default class AddToOrder extends Component<Props> {
@@ -22,9 +17,10 @@ export default class AddToOrder extends Component<Props> {
         super(props)
     
         this.state = {
+             
         }
-
     }
+    
 
     render() {
         return (
@@ -41,8 +37,13 @@ export default class AddToOrder extends Component<Props> {
                         </Row>
                     </Container>
                 </ModalHeader>
-                <Form onSubmit={(e) => this.props.addItemToOrder(e)}>
+                <Form onSubmit={this.props.addItemToOrder}>
                     <ModalBody>
+                        {/* Taken out until ability to add multiple of the same items to an order is complete */}
+                        {/* <FormGroup>
+                            <Label for='specialInstructions'>Special Instructions:</Label>
+                            <Input name='specialInstructions' id='specialInstructionsInput' required onChange={(e) => this.props.updateSpecialInstructions(e.target.value)} />
+                        </FormGroup> */}
                         <FormGroup>
                             <Label for='quantity'>Quantity:</Label>
                             <Input name='quantity' id='quantityInput' required onChange={(e) => this.props.updateQuantity(parseInt(e.target.value))} />
@@ -52,7 +53,71 @@ export default class AddToOrder extends Component<Props> {
                         <Button type='submit'>Done</Button>
                     </ModalFooter>
                 </Form>
+                
             </Modal>
         );
     }
 }
+
+
+
+
+
+// import React, { Component } from 'react';
+// import { Button, Form, FormGroup, Label, Input, Modal, ModalBody, ModalHeader, ModalFooter, Container, Row, Col } from "reactstrap";
+
+// import API_URL from '../../env';
+
+
+
+// type Props = {
+//     token: string,
+//     itemId: number,
+//     orderId: number,
+//     addToOrderOn: boolean,
+//     toggleAddToOrder: () => void,
+//     updateSpecialInstructions: (instructions: string) => void,
+//     updateQuantity: (quantity: number) => void,
+//     addItemToOrder: (e: React.FormEvent) => void
+// };
+
+// export default class AddToOrder extends Component<Props> {
+
+//     constructor(props: Props) {
+//         super(props)
+    
+//         this.state = {
+//         }
+
+//     }
+
+//     render() {
+//         return (
+//             <Modal isOpen={this.props.addToOrderOn}>
+//                 <ModalHeader>
+//                     <Container>
+//                         <Row>
+//                             <Col sm='10'>
+//                                 <h3>Add Item To Order</h3>
+//                             </Col>
+//                             <Col sm='2'>
+//                                 <Button color='danger' onClick={this.props.toggleAddToOrder}>X</Button>
+//                             </Col>
+//                         </Row>
+//                     </Container>
+//                 </ModalHeader>
+//                 <Form onSubmit={(e) => this.props.addItemToOrder(e)}>
+//                     <ModalBody>
+//                         <FormGroup>
+//                             <Label for='quantity'>Quantity:</Label>
+//                             <Input name='quantity' id='quantityInput' required onChange={(e) => this.props.updateQuantity(parseInt(e.target.value))} />
+//                         </FormGroup>
+//                     </ModalBody>
+//                     <ModalFooter>
+//                         <Button type='submit'>Done</Button>
+//                     </ModalFooter>
+//                 </Form>
+//             </Modal>
+//         );
+//     }
+// }
--- src/Components/MenuItem/MenuItem.tsx
+++ src/Components/MenuItem/MenuItem.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter as Router, Switch, Link} from 'react-router-dom';
 import {Button} from 'reactstrap';
 
 import API_URL from '../../env';
 import MenuItemEdit from './ItemEdit';
 import AddToOrder from './AddToOrder';
+
+import MenuItemModel from '../Models/MenuItemModel';
 
 import './MenuItem.css';
 
@@ -19,7 +22,8 @@ type Props = {
     },
     orderId: number,
     capName: (name: string) => string,
-    refreshMenu: () => void
+    refreshMenu: () => void,
+    // updateOrderItems: (item: MenuItemModel) => void
 };
 
 type State = {
@@ -48,74 +52,37 @@ export default class MenuItem extends Component<Props, State> {
         this.addItemToOrder = this.addItemToOrder.bind(this);
     }
 
-    // async toggleEdit(){
-    //     await this.setState({editOn: !this.state.editOn});
-    // }
-
-    toggleEdit = async() => await this.setState({editOn: !this.state.editOn});
+    async toggleEdit(){
+        await this.setState({editOn: !this.state.editOn});
+        // !this.state.editOn ? this.props.refreshMenu() : <></>;
+    }
 
     toggleAddToOrder = async() => await this.setState({addToOrderOn: !this.state.addToOrderOn});
 
     updateSpecialInstructions = async(instructions: string) => await this.setState({specialInstructions: instructions});
 
     updateQuantity = async(qunatity: number) => await this.setState({quantity: qunatity});
-
-    // async addItemToOrder(quantity: number){
-    //     try{
-    //         const url = `${API_URL}/order/food/${this.props.item.id}/add/${this.props.orderId}`;
-    //         const options = {
-    //             method: 'PUT',
-    //             body: JSON.stringify({
-    //                 specialInstructions: this.state.specialInstructions,
-    //                 quantity: quantity
-    //             }),
-    //             headers: new Headers({
-    //                 'Content-Type': 'application/json',
-    //                 'Authorization': this.props.token   
-    //             })
-    //         };
-
-    //         await fetch(url, options);
-    //         console.log('addItemToOrder() After Fetch');
-    //     }
-    //     catch(err){
-    //         console.log('Error:', err.message);
-    //     }
-    // }
     
-    async addItemToOrder(e: React.FormEvent){
-        e.preventDefault();
+    async addItemToOrder(){
+        try{
+            const url = `${API_URL}/order/food/${this.props.item.id}/add/${this.props.orderId}`;
+            const options = {
+                method: 'PUT',
+                body: JSON.stringify({
+                    specialInstructions: this.state.specialInstructions,
+                    quantity: this.state.quantity
+                }),
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Authorization': this.props.token   
+                })
+            };
 
-        if(!Number(this.state.quantity)){
-            alert('Enter a proper quantity amount.');
+            await fetch(url, options);
+            console.log('addItemToOrder() After Fetch');
         }
-        else{
-            try{
-                const url = `${API_URL}/order/food/${this.props.item.id}/add/${this.props.orderId}`;
-                const options = {
-                    method: 'PUT',
-                    body: JSON.stringify({
-                        specialInstructions: this.state.specialInstructions,
-                        quantity: this.state.quantity
-                    }),
-                    headers: new Headers({
-                        'Content-Type': 'application/json',
-                        'Authorization': this.props.token   
-                    })
-                };
-    
-                // await fetch(url, options);
-                const res = await fetch(url, options);
-                if(res.status != 200){
-                    const r = await res.json();
-                    console.log('R', r);
-                    // alert(`Error: ${r.Error.errors[0].message}`);
-                }
-                console.log('addItemToOrder() After Fetch');
-            }
-            catch(err){
-                console.log('Error:', err.message);
-            }
+        catch(err){
+            console.log('Error:', err.message);
         }
     }
 
@@ -123,18 +90,176 @@ export default class MenuItem extends Component<Props, State> {
         const {item} = this.props;
 
         return (
-            <div>
-                <h3>{this.props.capName(item.name)}</h3>
-                {/* .toFixed(2) => specifiy 2 decimal places */}
-                <h3>${this.props.item.price.toFixed(2)}</h3>
-                {localStorage.getItem('userRole') == 'admin' ? <Button onClick={this.toggleEdit}>Edit Item</Button> : <></>}
-                {this.props.orderId != 0 ? <Button onClick={this.toggleAddToOrder}>Add To Order</Button> : null}
-                {console.log('Menu Item Token:', this.props.token)}
-                {this.state.editOn ? <MenuItemEdit refreshMenu={this.props.refreshMenu} token={this.props.token} toggleEdit={this.toggleEdit} item={this.props.item} editOn={this.state.editOn} /> : null}
-                {this.state.addToOrderOn ? <AddToOrder token={this.props.token} itemId={this.props.item.id} orderId={this.props.orderId} addToOrderOn={this.state.addToOrderOn} toggleAddToOrder={this.toggleAddToOrder} updateSpecialInstructions={this.updateSpecialInstructions} updateQuantity={this.updateQuantity} addItemToOrder={this.addItemToOrder} /> : null}
-                {/* {this.state.addToOrderOn ? <AddToOrder token={this.props.token} itemId={this.props.item.id} orderId={this.props.orderId} addToOrderOn={this.state.addToOrderOn} toggleAddToOrder={this.toggleAddToOrder} updateSpecialInstructions={this.updateSpecialInstructions} addItemToOrder={this.addItemToOrder} /> : null} */}
-            </div>
+                <div>
+                    {/* <h3>{this.props.item.name}</h3> */}
+                    <h3>{this.props.capName(item.name)}</h3>
+                    {/* .toFixed(2) => specifiy 2 decimal places */}
+                    <h3>${this.props.item.price.toFixed(2)}</h3>
+                    {/* {this.props.user.isAdmin ? <Button onClick={this.toggleEdit}>Edit Item</Button> : <></>} */}
+                    {localStorage.getItem('userRole') == 'admin' ? <Button onClick={this.toggleEdit}>Edit Item</Button> : <></>}
+                    {/* {this.props.orderId != 0 ? <Button onClick={() => this.props.updateOrderItems(item)}>Add To Order</Button> : null} */}
+                    {/* {this.props.orderId != 0 ? <Button onClick={() => this.addItemToOrder(item)}>Add To Order</Button> : null} */}
+                    {this.props.orderId != 0 ? <Button onClick={this.toggleAddToOrder}>Add To Order</Button> : null}
+                    {console.log('Menu Item Token:', this.props.token)}
+                    {this.state.editOn ? <MenuItemEdit refreshMenu={this.props.refreshMenu} token={this.props.token} toggleEdit={this.toggleEdit} item={this.props.item} editOn={this.state.editOn} /> : null}
+                    {this.state.addToOrderOn ? <AddToOrder addToOrderOn={this.state.addToOrderOn} toggleAddToOrder={this.toggleAddToOrder} updateSpecialInstructions={this.updateSpecialInstructions} updateQuantity={this.updateQuantity} addItemToOrder={this.addItemToOrder} /> : null}
+                </div>
+
+            //     <Switch>
+            //         <Route path='/menuItem/itemId' exact>
+            //             <MenuItemEdit item={this.props.item} />
+            //         </Route>
+            //     </Switch>
+            // </Router>
         );
     }
 }
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// import {Button} from 'reactstrap';
+
+// import API_URL from '../../env';
+// import MenuItemEdit from './ItemEdit';
+// import AddToOrder from './AddToOrder';
+
+// import './MenuItem.css';
+
+
+
+// type Props = {
+//     user: {isAdmin: boolean},
+//     token: string,
+//     item: {
+//         id: number,
+//         name: string,
+//         price: number
+//     },
+//     orderId: number,
+//     capName: (name: string) => string,
+//     refreshMenu: () => void
+// };
+
+// type State = {
+//     editOn: boolean,
+//     addToOrderOn: boolean,
+//     specialInstructions: string,
+//     quantity: number
+// };
+
+// export default class MenuItem extends Component<Props, State> {
+
+//     constructor(props: Props) {
+//         super(props)
+    
+//         this.state = {
+//             editOn: false,
+//             addToOrderOn: false,
+//             specialInstructions: '',
+//             quantity: 0
+//         }
+
+//         this.toggleEdit = this.toggleEdit.bind(this);
+//         this.toggleAddToOrder = this.toggleAddToOrder.bind(this);
+//         this.updateSpecialInstructions = this.updateSpecialInstructions.bind(this);
+//         this.updateQuantity = this.updateQuantity.bind(this);
+//         this.addItemToOrder = this.addItemToOrder.bind(this);
+//     }
+
+//     // async toggleEdit(){
+//     //     await this.setState({editOn: !this.state.editOn});
+//     // }
+
+//     toggleEdit = async() => await this.setState({editOn: !this.state.editOn});
+
+//     toggleAddToOrder = async() => await this.setState({addToOrderOn: !this.state.addToOrderOn});
+
+//     updateSpecialInstructions = async(instructions: string) => await this.setState({specialInstructions: instructions});
+
+//     updateQuantity = async(qunatity: number) => await this.setState({quantity: qunatity});
+
+//     // async addItemToOrder(quantity: number){
+//     //     try{
+//     //         const url = `${API_URL}/order/food/${this.props.item.id}/add/${this.props.orderId}`;
+//     //         const options = {
+//     //             method: 'PUT',
+//     //             body: JSON.stringify({
+//     //                 specialInstructions: this.state.specialInstructions,
+//     //                 quantity: quantity
+//     //             }),
+//     //             headers: new Headers({
+//     //                 'Content-Type': 'application/json',
+//     //                 'Authorization': this.props.token   
+//     //             })
+//     //         };
+
+//     //         await fetch(url, options);
+//     //         console.log('addItemToOrder() After Fetch');
+//     //     }
+//     //     catch(err){
+//     //         console.log('Error:', err.message);
+//     //     }
+//     // }
+    
+//     async addItemToOrder(e: React.FormEvent){
+//         e.preventDefault();
+
+//         if(!Number(this.state.quantity)){
+//             alert('Enter a proper quantity amount.');
+//         }
+//         else{
+//             try{
+//                 const url = `${API_URL}/order/food/${this.props.item.id}/add/${this.props.orderId}`;
+//                 const options = {
+//                     method: 'PUT',
+//                     body: JSON.stringify({
+//                         specialInstructions: this.state.specialInstructions,
+//                         quantity: this.state.quantity
+//                     }),
+//                     headers: new Headers({
+//                         'Content-Type': 'application/json',
+//                         'Authorization': this.props.token   
+//                     })
+//                 };
+    
+//                 // await fetch(url, options);
+//                 const res = await fetch(url, options);
+//                 if(res.status != 200){
+//                     const r = await res.json();
+//                     console.log('R', r);
+//                     // alert(`Error: ${r.Error.errors[0].message}`);
+//                 }
+//                 console.log('addItemToOrder() After Fetch');
+//             }
+//             catch(err){
+//                 console.log('Error:', err.message);
+//             }
+//         }
+//     }
+
+//     render() {
+//         const {item} = this.props;
+
+//         return (
+//             <div>
+//                 <h3>{this.props.capName(item.name)}</h3>
+//                 {/* .toFixed(2) => specifiy 2 decimal places */}
+//                 <h3>${this.props.item.price.toFixed(2)}</h3>
+//                 {localStorage.getItem('userRole') == 'admin' ? <Button onClick={this.toggleEdit}>Edit Item</Button> : <></>}
+//                 {this.props.orderId != 0 ? <Button onClick={this.toggleAddToOrder}>Add To Order</Button> : null}
+//                 {console.log('Menu Item Token:', this.props.token)}
+//                 {this.state.editOn ? <MenuItemEdit refreshMenu={this.props.refreshMenu} token={this.props.token} toggleEdit={this.toggleEdit} item={this.props.item} editOn={this.state.editOn} /> : null}
+//                 {this.state.addToOrderOn ? <AddToOrder token={this.props.token} itemId={this.props.item.id} orderId={this.props.orderId} addToOrderOn={this.state.addToOrderOn} toggleAddToOrder={this.toggleAddToOrder} updateSpecialInstructions={this.updateSpecialInstructions} updateQuantity={this.updateQuantity} addItemToOrder={this.addItemToOrder} /> : null}
+//                 {/* {this.state.addToOrderOn ? <AddToOrder token={this.props.token} itemId={this.props.item.id} orderId={this.props.orderId} addToOrderOn={this.state.addToOrderOn} toggleAddToOrder={this.toggleAddToOrder} updateSpecialInstructions={this.updateSpecialInstructions} addItemToOrder={this.addItemToOrder} /> : null} */}
+//             </div>
+//         );
+//     }
+// }
 
